06d0bbc02fa45afdf117a03d85d4d85cf50f7ded
batu <ba921@nyu.edu>
2016-04-16 11:58:31 +0400

    index sentimetn additions

--- grocery/routes/index.js
+++ grocery/routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var sentimentAnalysis = require('sentiment-analysis');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+router.get('/post/analyze', function(req, res, next) {
+	
+
+	var tweet = "This horrible disgusting test tweet!";
+
+	var tweetSentiment = sentimentAnalysis(tweet2);
+
+
+});
+
 
 module.exports = router;
