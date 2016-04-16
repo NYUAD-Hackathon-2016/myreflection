c213a743a39926c02318a8b3a9f8cb65a5718e9e
Terri Burns <terri.burns@nyu.edu>
2016-04-16 16:08:50 +0400

    Updating files I think

--- grocery/routes/index.js
+++ grocery/routes/index.js
@@ -1,28 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var sentimentAnalysis = require('sentiment-analysis');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'My Expression' });
-});
-
-/* GET post page. */
-router.get('/post', function(req, res, next) {
-  var tweet = JSON.parse(req.body);
-  //POST TWEET
-  res.json({status: 'OK'});
-});
-
-
-router.get('/post/analyze', function(req, res, next) {
-	
-
-	var tweet = "This horrible disgusting test tweet!";
-
-	var tweetSentiment = sentimentAnalysis(tweet2);
-
-
 });
 
 
