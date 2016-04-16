d758d3c70dd7d1bd9f5467a1ffcc08bf79fd0864
batu <ba921@nyu.edu>
2016-04-16 15:39:28 +0400

    added on space bar ask for sentiment

---
+++ cordovaapp/www/js/analyze.js
@@ -0,0 +1,31 @@
+function getSentimentFromTweet(tweet){
+        console.log("I am in functiuo");
+        //var tempTweet = "Life is life. I am full of love!";
+        var sentimentValue = 10;
+        $.ajax({
+          type: "POST",
+          url: "http://localhost:3000/post",
+          data: JSON.stringify({ status: tweet }),
+          contentType: "application/json; charset=utf-8",
+          dataType: "json",
+          success: function(data) {
+            console.log("SUCCESS");
+            sentimentValue = data.status;
+            console.log(sentimentValue);
+          },
+          error: function(error) {
+            console.log("ERROR");
+            console.log(error);
+          }
+        });
+}
+
+window.addEventListener("keypress", checkKeyPressed, false);
+ 
+function checkKeyPressed(e) {
+    if (e.keyCode == "32") {
+      var text = $('#textBox').val();
+      console.log(text);
+      getSentimentFromTweet(text);
+    }
+}
