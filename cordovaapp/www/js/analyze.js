2a08595697648a6a46b97ceb19d6d38158939cef
batu <ba921@nyu.edu>
2016-04-16 17:54:41 +0400

    added color changing border

--- cordovaapp/www/js/analyze.js
+++ cordovaapp/www/js/analyze.js
@@ -1,7 +1,9 @@
+var sentimentValue;
+
 function getSentimentFromTweet(tweet){
         console.log("I am in functiuo");
-        //var tempTweet = "Life is life. I am full of love!";
-        var sentimentValue = 10;
+        //var tempTweet = "Life is life. I am full of love!";ue;
+
         $.ajax({
           type: "POST",
           url: "http://localhost:3000/post",
@@ -9,9 +11,8 @@ function getSentimentFromTweet(tweet){
           contentType: "application/json; charset=utf-8",
           dataType: "json",
           success: function(data) {
-            console.log("SUCCESS");
             sentimentValue = data.status;
-            console.log(sentimentValue);
+
           },
           error: function(error) {
             console.log("ERROR");
@@ -21,10 +22,20 @@ function getSentimentFromTweet(tweet){
 }
 
 window.addEventListener("keypress", checkKeyPressed, false);
+
+
  
 function checkKeyPressed(e) {
       var text = $('#textBox').val();
-      console.log(text);
       getSentimentFromTweet(text);
-  
+      console.log(sentimentValue);
+      $('#textBox').css( "border", "solid" );
+      if (sentimentValue > 0){
+        $('#textBox').css( "border-color", "green" );
+      } else if (sentimentValue < 0){
+        $('#textBox').css( "border-color", "red" );
+      }else{
+        $('#textBox').css( "border-color", "blue" );
+      }
+
 }
